21303b475b0f3af3b36d56e5df8e4f246d458e7e
Thang Truong <thang11a5tp@gmail.com>
2021-12-12 19:49:01 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
